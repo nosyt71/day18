9b8862086a9f6cebcedf6166026273ef99a02941
nosyt71 <tysonchan17@gmail.com>
2023-12-06 11:02:37 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/day18/Day18Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day18Application.class, args);
+	}
+
+}
---
+++ src/main/java/sg/edu/nus/iss/day18/controller/DemoController.java
@@ -0,0 +1,23 @@
+package sg.edu.nus.iss.day18.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/demo")
+public class DemoController {
+
+    @GetMapping(path="/hello")
+    public ResponseEntity<?> hello() {
+        try {
+            return new ResponseEntity<>("Hello!!! Springboot is okay...", HttpStatusCode.valueOf(200));
+        } catch (Exception ex) {
+            return new ResponseEntity<>("Error running Springboot...", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+}
